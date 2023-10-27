19119a13fafd835718e74ecc7abce5719658b868
Afnan-Abid <afnanabid765@gmail.com>
2023-10-27 15:31:58 -0700

    Full Work

---
+++ app.js
@@ -0,0 +1,59 @@
+
+/*var name1 = prompt("Enter your name");
+var age1 = prompt("Enter your age");
+var salary1 = prompt("Enter your salary");
+
+
+function names(name1 , age1 , salary1){
+  console.log("My name is " + name1 +  "and my age is " + age1 +  "and my salary is " + salary1 );
+}
+
+names(name1 , age1 , salary1);*/
+
+
+
+
+/*function sumOfTwo(num1 , num2 , num3){
+const sum = num1 + num2 - num3;
+return sum;
+
+}
+
+console.log(sumOfTwo(15 , 20 , 5 ));*/
+
+
+
+
+
+/*var names1= prompt("Enter your name");
+var product1 = prompt("Enter product name1");
+var product2 = prompt("Enter product name2");
+var price1 = prompt("Enter product1 price");
+var price2 = prompt+("Enter product2 price");
+
+
+
+function names(names1 , product1 , product2 , price1 , price2){
+  const names =   
+return names ("My name is rafay" + )
+}
+
+console.log(names1 , product1 , product2  ,price1  , price2 )*/
+
+
+
+
+
+var names1 = prompt("Enter your name");
+var product1 = prompt("Enter product name 1");
+var product2 = prompt("Enter product name 2");
+var price1 = Number(prompt("Enter product 1 price"));
+var price2 = Number(prompt("Enter product 2 price"));
+
+function Information(names1, product1, product2, price1, price2) {
+  return ("My name is " + names1 + "\nProduct 1: " + product1 + ", Price: " + price1 + "\nProduct 2: " + product2 + ", Price: " + price2);
+}
+
+var info = Information(names1, product1, product2, price1, price2);
+
+console.log(info);
